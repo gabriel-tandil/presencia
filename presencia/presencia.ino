e49828cc68d6adae9ace54287a81a6a369dbdee0
Gabriel <gabriel.tandil@gmail.com>
2016-10-20 10:35:42 -0300

    Mejoras varias, constantes,erc.

--- presencia/presencia.ino
+++ presencia/presencia.ino
@@ -1,27 +1,30 @@
 #include <DFPlayer_Mini_Mp3.h>
 #include <SPI.h>
 #include <WiFi.h>
+#include <DS3231.h> // incluyo esta que es mas liviana TODO: ver licencias
+#include <SoftwareSerial.h>
 //#include <Wire.h>
 //#include <Time.h>
 //#include <DS1307RTC.h>
-#include <DS3231.h> // incluyo esta que es mas liviana TODO: ver licencias
-#include <SoftwareSerial.h>
 
 
 DS3231  rtc(SDA, SCL);
 SoftwareSerial serialMp3(10, 11);
 const int INTERVALO_RELOJ = 100;
-const int MEDIA_ESCALA=512;
-const int ALERTA_VERDE=0;
-const int ALERTA_AMARILLA=1;
-const int ALERTA_NARANJA=2;
-const int ALERTA_ROJA=3;
+const int MEDIA_ESCALA = 512;
+const int ALERTA_VERDE = 0;
+const int ALERTA_AMARILLA = 1;
+const int ALERTA_NARANJA = 2;
+const int ALERTA_ROJA = 3;
+const int DEMORA_ENCENDIDO = 4;
+const int TIEMPO_ENCENDIDO = 60;
 
-int nivelAlerta=0;
-long milis;
+int nivelAlerta = 0;
+int lecturaAnterior = 0;
+long milisegundosReloj;
 long encender0 = 0, apagar0 = 0;
-long tiempo;
-int anterior = 0;
+long tiempoActual;
+
 
 void setup() {
   Serial.begin(9600);
@@ -30,27 +33,18 @@ void setup() {
   delay(1);  //wait 1ms for mp3 module to set volume
   mp3_set_volume (15);
   rtc.begin();
-  // rtc.setTime(13,29,00);
-  // rtc.setDate(20,4,2016);
-  // rtc.setDOW(3);
+
   pinMode(8, OUTPUT);//buzz
   pinMode(3, OUTPUT); //rele
 }
 
 void loop() {
-  if (millis() - milis > INTERVALO_RELOJ) {
+  if (millis() - milisegundosReloj > INTERVALO_RELOJ) {
     // procesar eventos de tiempo
-    milis = millis();
-    Time tiempoT = rtc.getTime();
-    //    Serial.print(tiempoT.hour);
-    //    Serial.print(":" );
-    //    Serial.print(tiempoT.min);
-    //    Serial.print(":");
-    //    Serial.println(tiempoT.sec );
-    // Serial.println(rtc.getTemp());
-    tiempo = rtc.getUnixTime(tiempoT);
+    milisegundosReloj = millis();
+    tiempoActual = rtc.getUnixTime(rtc.getTime());
 
-    if (encender0 <= tiempo && tiempo <= apagar0) {
+    if (encender0 <= tiempoActual && tiempoActual <= apagar0) {
       digitalWrite(3, LOW); //VALOR_ALTO_0
     }
     else {
@@ -61,23 +55,25 @@ void loop() {
   int valor = 0;
   valor = analogRead(A0);
   //  Serial.println(String(valor) +" "+ String(tiempo) + " "+String(encender0)+" "+String(apagar0));
-  Serial.println(valor);
+  // Serial.println(valor);
 
   delay(10);
 
-  int limite = analogRead(A1)/20;
+  int limite = analogRead(A1) / 20;
 
-  if (valor > MEDIA_ESCALA + limite  && anterior < MEDIA_ESCALA - limite) {
+  if (valor > MEDIA_ESCALA + limite  && lecturaAnterior < MEDIA_ESCALA - limite) {
     // Serial.println(String(valor) +" "+ String(tiempo) + " "+String(encender0)+" "+String(apagar0));
 
-    encender0 = tiempo + 4;
-    apagar0 = tiempo + 60;
-    tone(8,1000,100);
+    encender0 = tiempoActual + DEMORA_ENCENDIDO; // actua a los cuatro segundos
+    apagar0 = tiempoActual + TIEMPO_ENCENDIDO + DEMORA_ENCENDIDO; //por un minuto y apaga
+    tone(8, 1000, 100);
   }
-  anterior = valor;
+  lecturaAnterior = valor;
+
+  //este seria el ideal a lograr para modularizacion
+  //---------------------------------------------
   //  if (condicion(valor)){
   //   procesarReglas()
   // }
-
 
 }
